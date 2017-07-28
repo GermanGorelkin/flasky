e9b7ef1430d966a2abff73ab5489d19822281b9f
German Gorelkin <german.gorelkin@gmail.com>
2017-07-28 18:49:13 +0300

    change Form to FlaskForm

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
